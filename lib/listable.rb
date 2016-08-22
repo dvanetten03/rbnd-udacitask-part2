b01a8e49de2b4034fea0bde64f34c8fed2393133
Diane Van Etten <diane.vanetten@gmail.com>
2016-08-22 11:16:45 -0500

    First attempt at format_date and add format_priority

--- lib/listable.rb
+++ lib/listable.rb
@@ -2,4 +2,26 @@ module Listable
   def format_description(description)
     "#{description}".ljust(30)
   end
+
+  def format_priority(priority)
+    value = " ⇧" if priority == "high"
+    value = " ⇨" if priority == "medium"
+    value = " ⇩" if priority == "low"
+    value = "" if !priority
+    return value
+  end
+
+  def format_date(options{})
+    due = options[:due] ? Date.parse(options[:due]) : options[:due]
+    start_date = Date.parse(options[:start_date]) if options[:start_date]
+    end_date = Date.parse(options[:end_date]) if options[:end_date]
+
+    dates = @start_date.strftime("%D") if @start_date
+    dates << " -- " + @end_date.strftime("%D") if @end_date
+    dates = "N/A" if !dates
+    return dates
+
+    @due ? @due.strftime("%D") : "No due date"
+
+    
 end
